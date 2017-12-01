2067465cb7d35677dc8d9ce5581c9dbd0f8a2e9c
Henderson Hood <Henderson@eriethamespower.com>
2017-12-01 14:35:24 -0500

    Just a test commit on attributes.rb file

--- cookbooks/myhttpd/attributes/attributes.rb
+++ cookbooks/myhttpd/attributes/attributes.rb
@@ -2,3 +2,5 @@ default['web']['document_root'] = '/var/www/html'
 default['web']['log_dir'] = '/var/log/httpd-default'
 default['web']['home_page'] = '/var/www/html/index.html'
 default['httpd']['service_name'] = 'default'
+
+#this is just a test
